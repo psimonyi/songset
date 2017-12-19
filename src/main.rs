e35b230efaacc946d50d6ac9471f4fe01d769e90
Peter Simonyi <pts@petersimonyi.ca>
2017-12-19 15:16:41 -0500

    Mostly working tr step after parsing

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,14 @@ fn main() {
         let mut file = fs::File::open(path).unwrap();
         let mut s = String::new();
         file.read_to_string(&mut s).unwrap();
+        let parsed = parse::song(&s).unwrap();
+        let song = tr_song(&parsed);
 
-        println!("{:?}", parse::song(&s));
+        println!("{:?}", song);
     }
 }
+
+// Structs produced by the parser:
 
 #[derive(Debug)]
 pub struct Line<'a> {
@@ -41,41 +45,35 @@ enum Item<'a> {
     Sexp(Sexp<'a>),
 }
 
-/*
+// Structs produced after translation:
+
 #[derive(Debug)]
 struct Song {
     meta: Vec<Metadata>,
     verses: Vec<Verse>,
 }
 
-/*
- * a verse can be a list of lines, each of which is formatted text
- * the verse has a number or is a chorus or has a label
- * is the verse indented or are lines indented or both? probably lines.
- * formatted text is another matter though
- * and titles or other metadata could contain that
- */
 #[derive(Debug)]
 struct Verse {
-    lines: Vec<Line>,
-    label: String,
+    lines: Vec<FormattedText>,
     style: VerseType,
 }
 
 #[derive(Debug)]
 enum VerseType {
-    Refrain,
-    Numbered,
-    Labelled,
+    Normal,
+    Refrain(String), // e.g. "Chorus:"
+    ChorusInstance(String), // e.g. "Chorus"
 }
 
 struct FormattedText {
     text: String,
     formatting: pango::AttrList,
+    indent: u32,
 }
 impl std::fmt::Debug for FormattedText {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-        write!(f, "FormattedText({:?})", self.text)
+        write!(f, "FormattedText({:?}, {})", self.text, self.indent)
     }
 }
 
@@ -102,10 +100,98 @@ enum Metadata {
     Unknown,
 }
 
-fn parse_formatted_text(src: &Vec<Item>) -> FormattedText {
+fn tr_song(src: &Vec<Vec<Line>>) -> Result<Song, Error> {
+    let mut i = src.iter();
+    let meta = tr_meta_block(i.next().unwrap());
+    let verses = i.filter_map(tr_verse).collect();
+
+    Song { meta, verses }
+}
+
+fn tr_meta_block(src: &Vec<Line>) -> Vec<Metadata> {
+    src.iter().flat_map(|l| &l.items).filter_map(|item| {
+        match *item {
+            Item::Text(s) => {
+                assert!(str_is_whitespace(s));
+                None
+            },
+            Item::Sexp(ref sexp) => {
+                Some(tr_meta_entry(sexp))
+            },
+        }
+    }).collect()
+}
+
+fn str_is_whitespace(s: &str) -> bool {
+    s.chars().all(char::is_whitespace)
+}
+
+fn tr_meta_entry(sexp: &Sexp) -> Metadata {
+    match sexp.keyword {
+        "title" => Metadata::Title(tr_formatted_text(&sexp.items)),
+        "alt-title" => Metadata::AltTitle(tr_formatted_text(&sexp.items)),
+        _ => Metadata::Unknown,
+        //_ => panic!("Unrecognized keyword"),
+    }
+}
+
+fn tr_verse(src: &Vec<Line>) -> Option<Verse> {
+    let mut i = src.iter().peekable();
+    let style = {
+        let line = i.peek().unwrap(); // It's the first; there must be one.
+        tr_verse_meta(line)
+    };
+    if style.is_some() {
+        // We used the peeked line so advance the iterator.
+        i.next();
+    }
+
+    let lines = i.map(tr_line).collect();
+    Some(Verse {
+        lines,
+        style: style.unwrap_or(VerseType::Normal),
+    })
+}
+
+fn tr_verse_meta(line: &Line) -> Option<VerseType> {
+    let mut rv = None;
+    for item in &line.items {
+        match *item {
+            Item::Text(ref s) => {
+                if !str_is_whitespace(s) { return None; }
+            },
+            Item::Sexp(ref sexp) => {
+                if rv.is_some() { return None; }
+                match *sexp {
+                    Sexp { keyword: "Chorus:", ref items }
+                    if items.is_empty() => {
+                        rv = Some(VerseType::Refrain(String::from("Chorus")));
+                    },
+                    Sexp { keyword: "Refrain:", ref items }
+                    if items.is_empty() => {
+                        rv = Some(VerseType::Refrain(String::from("Refrain")));
+                    },
+                    Sexp { keyword: "Chorus", ref items }
+                    if items.is_empty() => {
+                        rv = Some(VerseType::ChorusInstance(String::from("Chorus")));
+                    },
+                    _ => return None,
+                }
+            },
+        }
+    }
+    rv
+}
+
+fn tr_line(src: &Line) -> FormattedText {
+    tr_formatted_text(&src.items)
+}
+
+fn tr_formatted_text(src: &Vec<Item>) -> FormattedText {
     let mut ft = FormattedText {
         text: String::new(),
         formatting: pango::AttrList::new(),
+        indent: 0,
     };
     add_formatted_text(src, &mut ft);
     ft
@@ -115,36 +201,18 @@ fn add_formatted_text(src: &Vec<Item>, ft: &mut FormattedText) {
     for item in src {
         match *item {
             Item::Text(ref s) => ft.text.push_str(s),
-            Item::Sexp(ref s) => {
-                match s.keyword {
-                    "italic" => {
-                        // When would new_style ever return None???
-                        let mut attr = pango::Attribute::new_style(pango::Style::Italic).unwrap();
-                        attr.set_start_index(ft.text.len() as u32);
-                        add_formatted_text(&s.items, ft);
-                        attr.set_end_index(ft.text.len() as u32);
-                        ft.formatting.change(attr);
-                    },
-                    _ => panic!("bad keyword {}", s.keyword),
-                }
+            Item::Sexp(Sexp{keyword: "italic", ref items}) => {
+                // When would new_style ever return None???
+                let mut attr = pango::Attribute::new_style(pango::Style::Italic).unwrap();
+                attr.set_start_index(ft.text.len() as u32);
+                add_formatted_text(items, ft);
+                attr.set_end_index(ft.text.len() as u32);
+                ft.formatting.change(attr);
             },
+            ref x => panic!("bad item {:?}", x),
         }
     }
 }
-*/
-
-/*
-fn make_meta_entry<I>(keyword: &str, args: Pairs<Rule, I>) -> Metadata
-where I: Input {
-    match keyword {
-        "title" => Metadata::Title(parse_formatted_text(args)),
-        "alt-title" => Metadata::AltTitle(parse_formatted_text(args)),
-        _ => Metadata::Unknown,
-        //_ => panic!("Unrecognized keyword"),
-    }
-}
-
-*/
 
 /*
 author - should just use attrib
